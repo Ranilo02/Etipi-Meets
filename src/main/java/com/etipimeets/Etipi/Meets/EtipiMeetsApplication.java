2dcbabd5e5330be743f80fa1d9c7a8704ff3fb1f
Ranilo <ranilocabral@gmail.com>
2024-06-28 12:55:07 -0300

    Initial Commit

---
+++ src/main/java/com/etipimeets/Etipi/Meets/EtipiMeetsApplication.java
@@ -0,0 +1,13 @@
+package com.etipimeets.Etipi.Meets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtipiMeetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtipiMeetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/etipimeets/Etipi/Meets/EtipiMeetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etipimeets.Etipi.Meets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtipiMeetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
